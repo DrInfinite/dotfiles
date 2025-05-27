39aee45173cb51c3153ba35d5d93b16934760a22
DrInfinite <naparajith@duck.com>
2025-05-27 10:57:52 +0530

    instal and setup emacs
    - install emacs
    - setup alias for doom
    - install doom emacs

---
+++ bash/scripts/setup/setup_emacs.sh
@@ -0,0 +1,5 @@
+# Clone Doom Emacs repository
+git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
+
+# Run Doom Emacs installer
+~/.emacs.d/bin/doom install
--- bash/scripts/setup/setup_programs.sh
+++ bash/scripts/setup/setup_programs.sh
@@ -1,1 +1,2 @@
 sudo pacman -S btop nvim fastfetch zellij zoxide git lazygit fzf ripgrep fd eza
+sudo pacman -S emacs
