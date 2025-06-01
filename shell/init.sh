d2645b69694f38321fe084ac4518b09d96c7aca3
DrInfinite <naparajith@duck.com>
2025-06-01 17:25:53 +0530

    fixed zoxide init issue

--- shell/init.sh
+++ shell/init.sh
@@ -1,7 +1,7 @@
 eval "$(fzf --bash)"
 eval "$(zellij setup --generate-auto-start bash)"
 eval "$(mise activate bash)"
-eval "$(zoxide init bash)"
 eval "$(starship init bash)"
+eval "$(zoxide init bash)"
 
 fastfetch
