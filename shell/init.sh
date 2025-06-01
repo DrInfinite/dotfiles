e2f4fa0a77a0b38eddcc685026e1fd6a22c7e92b
DrInfinite <naparajith@duck.com>
2025-06-01 17:16:41 +0530

    migrating to starship from a custom prompt

--- shell/init.sh
+++ shell/init.sh
@@ -2,5 +2,6 @@ eval "$(fzf --bash)"
 eval "$(zellij setup --generate-auto-start bash)"
 eval "$(mise activate bash)"
 eval "$(zoxide init bash)"
+eval "$(starship init bash)"
 
 fastfetch
--- shell/rc.sh
+++ shell/rc.sh
@@ -1,4 +1,3 @@
 source ~/shell/aliases.sh
-source ~/shell/prompt.sh
 source ~/shell/init.sh
 source ~/shell/colours.sh
