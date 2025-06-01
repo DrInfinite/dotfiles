05af204a1b526846aa159507a1185aba380ec084
DrInfinite <naparajith@duck.com>
2025-06-01 15:20:42 +0530

    shell neutral naming scheme
    - ensuring future path of migration to different shells

--- bash/rc.sh
+++
@@ -1,4 +0,0 @@
-source ~/bash/aliases.sh
-source ~/bash/prompt.sh
-source ~/bash/init.sh
-source ~/bash/colours.sh
---
+++ shell/rc.sh
@@ -0,0 +1,4 @@
+source ~/shell/aliases.sh
+source ~/shell/prompt.sh
+source ~/shell/init.sh
+source ~/shell/colours.sh
