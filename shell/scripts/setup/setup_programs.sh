8f79156ed305eac7786897a6836554ac68077d97
DrInfinite <naparajith@duck.com>
2025-06-03 13:28:14 +0530

    setup yazi: a terminal file manager

--- shell/scripts/setup/setup_programs.sh
+++ shell/scripts/setup/setup_programs.sh
@@ -1,2 +1,6 @@
 sudo pacman -S firefox thunderbird alacritty
 sudo pacman -S btop nvim fastfetch zellij zoxide git lazygit fzf ripgrep fd eza starship bat wikiman
+
+# File Manager
+sudo pacman -S yazi ffmpeg 7zip jq poppler imagemagick wl-clipboard gvfs glib2 gvfs-google
+yay -S resvg
