53ff5bab9ec2d29a754dcc3b7a42fe0fcc386ca6
DrInfinite <naparajith@duck.com>
2025-06-12 02:15:01 +0530

    improved emacs setup process

--- shell/scripts/setup/setup_emacs.sh
+++ shell/scripts/setup/setup_emacs.sh
@@ -1,12 +1,18 @@
-sudo pacman -S emacs
+sudo pacman -S emacs gum
 
 rm -rf ~/.emacs.d
+
+gum confirm "Are you ready to clone the Doom Emacs repository?"
 
 # Clone Doom Emacs repository
 git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
 
+gum confirm "Would you like to run 'doom install' now?"
+
 # Run Doom Emacs installer
 ~/.emacs.d/bin/doom install
+
+gum confirm "Would you like to run 'doom sync' now?"
 
 # Run Doom Emacs sync for custom packages
 ~/.emacs.d/bin/doom sync
