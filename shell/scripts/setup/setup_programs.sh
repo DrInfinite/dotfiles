a5a3881928307b970b3bfef87eaf9e9648a28376
DrInfinite <naparajith@duck.com>
2025-06-12 02:49:59 +0530

    setup command line music player

--- shell/scripts/setup/setup_programs.sh
+++ shell/scripts/setup/setup_programs.sh
@@ -1,5 +1,5 @@
 sudo pacman -S firefox thunderbird alacritty
-sudo pacman -S btop nvim fastfetch zellij zoxide git lazygit fzf ripgrep fd eza starship bat wikiman
+sudo pacman -S btop nvim fastfetch zellij zoxide git lazygit fzf ripgrep fd eza starship bat wikiman kew
 
 # File Manager
 sudo pacman -S yazi ffmpeg 7zip jq poppler imagemagick wl-clipboard gvfs glib2 gvfs-google
