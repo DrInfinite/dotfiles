4d0387c66a2ac289e917fb7501d83f12b672c2ff
DrInfinite <naparajith@duck.com>
2025-06-24 20:07:08 +0530

    setup zsh and yazi

---
+++ shell/scripts/setup/programs/setup_yazi.sh
@@ -0,0 +1,3 @@
+# Setup Yazi
+sudo pacman -S yazi ffmpeg 7zip jq poppler imagemagick wl-clipboard gvfs glib2 gvfs-google --needed
+yay -S resvg --needed
--- shell/scripts/setup/setup_arch.sh
+++ shell/scripts/setup/setup_arch.sh
@@ -1,7 +1,9 @@
 # Perform a full initial system update
 sudo pacman -Syu
 
+# Setup the shell
 ~/shell/scripts/setup/setup_yay.sh
+~/shell/scripts/setup/setup_zsh.sh
 ~/shell/scripts/setup/setup_font.sh
 ~/shell/scripts/setup/setup_firmware.sh
 ~/shell/scripts/setup/setup_programs.sh
@@ -11,6 +13,9 @@ sudo pacman -Syu
 ~/shell/scripts/setup/setup_gnome_settings.sh
 ~/shell/scripts/setup/setup_gnome_extensions.sh
 ~/shell/scripts/setup/setup_gnome_keymaps.sh
+
+gum confirm "Do you want to install Yazi, a blazingly fast rust-based terminal file manager?"
+~/shell/scripts/setup/programs/setup_yazi.sh
 
 gum confirm "Do you want to install Emacs, an extensible, customizable, free/libre text editor?"
 ~/shell/scripts/setup/programs/setup_emacs.sh
