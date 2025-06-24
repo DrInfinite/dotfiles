4a482be3b844d83b6c6e596183fe727b10dbe92c
DrInfinite <naparajith@duck.com>
2025-06-24 20:07:35 +0530

    move yazi setup to its own file

--- shell/scripts/setup/setup_programs.sh
+++ shell/scripts/setup/setup_programs.sh
@@ -1,6 +1,2 @@
-sudo pacman -S firefox thunderbird alacritty
-sudo pacman -S btop nvim fastfetch zellij zoxide git lazygit fzf ripgrep fd eza starship bat wikiman kew tokei
-
-# File Manager
-sudo pacman -S yazi ffmpeg 7zip jq poppler imagemagick wl-clipboard gvfs glib2 gvfs-google
-yay -S resvg
+sudo pacman -S firefox thunderbird alacritty --needed
+sudo pacman -S btop nvim fastfetch zellij zoxide git lazygit fzf ripgrep fd eza starship bat wikiman kew tokei gum --needed
