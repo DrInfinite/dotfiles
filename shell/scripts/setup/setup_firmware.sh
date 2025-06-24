5d7d4cd4fb714e10257488f40ef900f06a4568fc
DrInfinite <naparajith@duck.com>
2025-06-24 20:07:17 +0530

    new firmware packages

--- shell/scripts/setup/setup_firmware.sh
+++ shell/scripts/setup/setup_firmware.sh
@@ -1,2 +1,2 @@
-sudo pacman -S linux-firmware-qlogic linux-firmware-bnx2x linux-firmware-liquidio linux-firmware-mellanox linux-firmware-nfp linux-firmware-qlogic linux-headers libvpl vpl-gpu-rt
-yay -S aic94xx-firmware ast-firmware wd719x-firmware upd72020x-fw
+sudo pacman -S linux-firmware-qlogic linux-firmware-bnx2x linux-firmware-liquidio linux-firmware-mellanox linux-firmware-nfp linux-firmware-qlogic linux-headers libvpl vpl-gpu-rt --needed
+yay -S aic94xx-firmware ast-firmware wd719x-firmware upd72020x-fw --needed
