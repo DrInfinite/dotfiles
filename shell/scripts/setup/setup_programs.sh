39cef939af95dd0fb0310da286ba60f25afb7055
DrInfinite <naparajith@duck.com>
2025-06-28 05:04:13 +0530

    setup programs

--- shell/scripts/setup/setup_programs.sh
+++ shell/scripts/setup/setup_programs.sh
@@ -1,2 +1,2 @@
-sudo pacman -S firefox thunderbird alacritty --needed
-sudo pacman -S btop nvim fastfetch zellij zoxide git lazygit fzf ripgrep fd eza starship bat wikiman kew tokei gum --needed
+sudo pacman -S firefox thunderbird alacritty bitwarden --needed
+sudo pacman -S btop nvim fastfetch zellij zoxide git lazygit fzf ripgrep fd eza starship bat wikiman tokei gum --needed
