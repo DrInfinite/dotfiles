5421f8b5a482b1f563cd5c22084101385db73085
DrInfinite <naparajith@duck.com>
2025-07-10 15:31:09 +0530

    refactor zinit config

--- .zshrc
+++ .zshrc
@@ -1,13 +1,3 @@
-# Set the directory we want to store zinit and plugins
-ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
-
-# Check if zinit exists, if not, download and set it up
-[ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
-[ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
-
-# Source zinit
-source "${ZINIT_HOME}/zinit.zsh"
-
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
 
--- shell/zsh/init.sh
+++ shell/zsh/init.sh
@@ -1,3 +1,13 @@
+# Set the directory we want to store zinit and plugins
+ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+
+# Check if zinit exists, if not, download and set it up
+[ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
+[ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+
+# Source zinit
+source "${ZINIT_HOME}/zinit.zsh"
+
 eval "$(fzf --zsh)"
 eval "$(zellij setup --generate-auto-start zsh)"
 eval "$(mise activate zsh)"
