b9fa67dcb623b424199f31dd8449c4d197c15826
DrInfinite <naparajith@duck.com>
2025-08-27 20:02:05 +0530

    nvim: new todo highlights

---
+++ .config/nvim/lua/plugins/highlights.lua
@@ -0,0 +1,15 @@
+local todo_comments = {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+        keywords = {
+            DONE = {
+                icon = "󰔓 ",
+                color = "info",
+                alt = { "COMPLETED", "FINISHED" },
+            },
+        },
+    },
+}
+
+return todo_comments
