998d8b20a7d548e585b32df14c0c3491cafdc4ca
DrInfinite <naparajith@duck.com>
2025-09-16 10:33:28 +0530

    zsh: formatting

--- shell/zsh/shell.sh
+++ shell/zsh/shell.sh
@@ -9,11 +9,11 @@ bindkey -e
 source /usr/share/wikiman/widgets/widget.zsh
 
 if [ -n "$ZELLIJ" ]; then
-  # Inside Zellij: override PROMPT_COMMAND to do nothing
-  PROMPT_COMMAND=''
+    # Inside Zellij: override PROMPT_COMMAND to do nothing
+    PROMPT_COMMAND=''
 else
-  # Regular title setting for normal terminals
-  PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD}\007"'
+    # Regular title setting for normal terminals
+    PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD}\007"'
 fi
 
 # Set complete path
