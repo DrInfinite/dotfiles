4ada5168ae247b7a931bc040791a295af7accbfe
DrInfinite <naparajith@duck.com>
2025-10-07 09:42:56 +0530

    neovim: changed default tab stops to 2

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,5 @@
+---@diagnostic disable: undefined-global
+
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
@@ -8,5 +10,5 @@ vim.cmd([[set guicursor=n-v-c-sm-i-ci:block,ve:ver25,r-cr-o:hor20]])
 
 -- Use spaces as default over tabs
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
