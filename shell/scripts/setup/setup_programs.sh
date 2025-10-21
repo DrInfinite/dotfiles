cfe8c7753c7dcc6cc508d154dd258050e8afd18d
DrInfinite <naparajith@duck.com>
2025-10-21 10:45:16 +0530

    shell: switch back to alacritty as the default

--- shell/scripts/setup/setup_programs.sh
+++ shell/scripts/setup/setup_programs.sh
@@ -1,4 +1,4 @@
-sudo pacman -S thunderbird kitty bitwarden --needed
+sudo pacman -S thunderbird alacritty bitwarden --needed
 sudo pacman -S btop nvim fastfetch tmux zoxide git lazygit fzf ripgrep fd eza starship bat wikiman tokei gum --needed
 
 yay -S zen-browser-bin
