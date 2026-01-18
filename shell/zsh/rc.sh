1ea3b616dee6ed045d0937d41c0e8692dc656f29
DrInfinite <naparajith@duck.com>
2026-01-18 23:42:45 +0530

    zsh: add all zsh modifications to the rc file

--- shell/zsh/rc.sh
+++ shell/zsh/rc.sh
@@ -1,8 +1,10 @@
 source "$HOME"/shell/aliases.sh
-source "$HOME"/shell/functions.sh
-source "$HOME"/shell/zsh/init.sh
-# source ~/shell/colours.sh
-source "$HOME"/shell/zsh/shell.sh
-source "$HOME"/shell/zsh/plugins.sh
-source "$HOME"/shell/zsh/options.sh
 source "$HOME"/shell/env.sh
+source "$HOME"/shell/functions.sh
+source "$HOME"/shell/zsh/aliases.sh
+source "$HOME"/shell/zsh/hooks.sh
+source "$HOME"/shell/zsh/init.sh
+source "$HOME"/shell/zsh/keybinds.sh
+source "$HOME"/shell/zsh/options.sh
+source "$HOME"/shell/zsh/plugins.sh
+source "$HOME"/shell/zsh/shell.sh
