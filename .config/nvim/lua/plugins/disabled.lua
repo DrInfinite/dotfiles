c97c802db52c6dab71fd9dbb64cbe29ed54a5f9a
DrInfinite <naparajith@duck.com>
2026-03-27 08:45:10 +0530

    nvim: updated config formatting

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 local disabled = {
-    { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
 
 return disabled
--- .config/nvim/lua/plugins/highlights.lua
+++ .config/nvim/lua/plugins/highlights.lua
@@ -1,15 +1,15 @@
 local todo_comments = {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-        keywords = {
-            DONE = {
-                icon = "󰔓 ",
-                color = "info",
-                alt = { "COMPLETED", "FINISHED" },
-            },
-        },
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    keywords = {
+      DONE = {
+        icon = "󰔓 ",
+        color = "info",
+        alt = { "COMPLETED", "FINISHED" },
+      },
     },
+  },
 }
 
 return todo_comments
