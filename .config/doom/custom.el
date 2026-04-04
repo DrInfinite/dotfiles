6e3eca9b01843567d81d470774fc31f6b3988f06
DrInfinite <naparajith@duck.com>
2026-04-04 20:21:26 +0530

    emacs: custom doom configs

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -4,6 +4,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("8f5b54bf6a36fe1c138219960dd324aad8ab1f62f543bed73ef5ad60956e36ae" default))
  '(elfeed-feeds
    '("https://developer.nvidia.com/blog//feed" "https://techcrunch.com/feed/")))
 (custom-set-faces
@@ -11,6 +13,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(markdown-header-face ((t (:inherit font-lock-function-name-face :family 'doom-font))))
+ '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.6))))
+ '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.5))))
+ '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 1.4))))
+ '(markdown-header-face-4 ((t (:inherit markdown-header-face :height 1.3))))
+ '(markdown-header-face-5 ((t (:inherit markdown-header-face :height 1.2))))
+ '(markdown-header-face-6 ((t (:inherit markdown-header-face :height 1.1))))
  '(org-document-info ((t (:inherit default :height 1.3 :weight semibold))))
  '(org-document-info-keyword ((t (:inherit default :height 1.2 :weight medium))))
  '(org-document-title ((t (:inherit default :height 1.75))))
